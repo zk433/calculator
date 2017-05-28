a3d5aa693b37a72888bc42d474a34983443e30b3
Zuzana K <zk0433@gmail.com>
2017-05-28 21:56:00 +0100

    user interaction and business logic finished for add, subtract, multiply, divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
+$(document).ready(function(){
+	// logic responsible for user front -end / interaction only
+	$("form#add").submit(function(event){
+		event.preventDefault();
+		var num1 = parseInt($("#add1").val());
+		var num2 = parseInt($("#add2").val());
+		var result = add(num1, num2);
+		$("#output").text(result);
+	});
+
+	$("form#subtract").submit(function(event){
+		event.preventDefault();
+		var num1 = parseInt($("#subtract1").val());
+		var num2 = parseInt($("#subtract2").val());
+		var result = subtract(num1, num2);
+		$("#output").text(result);
+	});
+
+	$("form#multiply").submit(function(event){
+		event.preventDefault();
+		var num1 = parseInt($("#multiply1").val());
+		var num2 = parseInt($("#multiply2").val());
+		var result = multiply(num1, num2);
+		$("#output").text(result);
+	});
+
+	$("form#divide").submit(function(event){
+		event.preventDefault();
+		var num1 = parseInt($("#divide1").val());
+		var num2 = parseInt($("#divide2").val());
+		var result = divide(num1, num2);
+		$("#output").text(result);
+	});
+});
+
+// back-end / Business logic
 //add, subtract, multiply, and divide functions
 
 var add = function(num1, num2){
@@ -12,15 +48,10 @@ var multiply = function(num1, num2){
 	return num1 * num2;
 };
 
-var divide = funtion(num1, num2){
+var divide = function(num1, num2){
 	return num1 / num2;
 };
 
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter another number:"));
 
-var result = add(num1, num2);
-
-alert(result);
 
 
